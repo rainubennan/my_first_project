e23456bd0c99ac5b7598891dde475608533b0734
deept <rainubennan01@gmail.com>
2022-12-21 10:29:37 +0530

    commit

--- lib/Nov/list.dart
+++ lib/Nov/list.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
+void main(){
+  runApp(MaterialApp(home: L_ist(),debugShowCheckedModeBanner: false,));
+}
 
 class L_ist extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(appBar: AppBar(),
+    body: Center(child: Text("hello")),);
   }
 
 }
